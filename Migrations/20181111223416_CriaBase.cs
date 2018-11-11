d50ec9eb9e03d69ba239f96aa9556faedf3fb355
marcelo campos <marcelocampos190098@gmail.com>
2018-11-11 19:54:01 -0300

    adicionando migration

---
+++ Migrations/20181111223416_CriaBase.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace easyhotelbackend.Migrations
+{
+    public partial class CriaBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    HotelId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Descricao = table.Column<string>(nullable: true),
+                    Nome = table.Column<string>(nullable: true),
+                    Cidade = table.Column<string>(nullable: true),
+                    Estado = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Avaliacao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.HotelId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    UsuarioId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Nome = table.Column<string>(nullable: true),
+                    Senha = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.UsuarioId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Quarto",
+                columns: table => new
+                {
+                    QuartoId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    HotelId = table.Column<int>(nullable: false),
+                    TipoQuarto = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: true),
+                    AvaliacaoQuarto = table.Column<string>(nullable: true),
+                    Valor = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quarto", x => x.QuartoId);
+                    table.ForeignKey(
+                        name: "FK_Quarto_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "HotelId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Imagem",
+                columns: table => new
+                {
+                    ImagemId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Nome = table.Column<string>(nullable: true),
+                    Arquivo = table.Column<string>(nullable: true),
+                    QuartoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Imagem", x => x.ImagemId);
+                    table.ForeignKey(
+                        name: "FK_Imagem_Quarto_QuartoId",
+                        column: x => x.QuartoId,
+                        principalTable: "Quarto",
+                        principalColumn: "QuartoId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reserva",
+                columns: table => new
+                {
+                    ReservaId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    UsuarioId = table.Column<int>(nullable: false),
+                    QuartoId = table.Column<int>(nullable: false),
+                    DataInicio = table.Column<DateTime>(nullable: false),
+                    DataFim = table.Column<DateTime>(nullable: false),
+                    Valor = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reserva", x => x.ReservaId);
+                    table.ForeignKey(
+                        name: "FK_Reserva_Quarto_QuartoId",
+                        column: x => x.QuartoId,
+                        principalTable: "Quarto",
+                        principalColumn: "QuartoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Imagem_QuartoId",
+                table: "Imagem",
+                column: "QuartoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quarto_HotelId",
+                table: "Quarto",
+                column: "HotelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reserva_QuartoId",
+                table: "Reserva",
+                column: "QuartoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Imagem");
+
+            migrationBuilder.DropTable(
+                name: "Reserva");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Quarto");
+
+            migrationBuilder.DropTable(
+                name: "Hotels");
+        }
+    }
+}
